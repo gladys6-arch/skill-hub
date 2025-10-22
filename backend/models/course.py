82dcffd33bf1bcc09260115315a33ff347898531
gladys <achandogladys6@gmail.com>
2025-10-22 21:30:51 +0300

    enrollment relationships

--- backend/models/course.py
+++ backend/models/course.py
@@ -26,9 +26,21 @@ class Module(db.Model):
     content = db.Column(db.Text)
     course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
 
+    #relationship
+    course = db.relationship('Course', back_populates='modules')
+
+
 class Enrollment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     student_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
+    date_enrolled = db.Column(db.DateTime,  default=db.func.now())
     progress = db.Column(db.Integer, default=0)
     completed = db.Column(db.Boolean, default=False)
+
+    #relationships
+    student = db.relationship('User', back_populates='enrollments')
+    course = db.relationship('Course', back_populates='enrollments')
+
+
+
