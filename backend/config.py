59e16eacfc754fac1b0428b33bfa6939a44a0439
unknown <akaidylan81@gmail.com>
2025-10-23 14:19:50 +0100

    config changes

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,3 @@
-import os
-
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///skillhub.db'
