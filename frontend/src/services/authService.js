662bca14ebde13e65bf6ee89d187d27fafc315df
George-alt5 <georgekihoro6@gmail.com>
2025-10-28 12:10:32 +0300

    update services and dependencies for frontend integration

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,5 +1,6 @@
-import axios from 'axios';
-import { API_BASE_URL } from './api';
+import axios from "axios";
+import { API_BASE_URL } from "../api";
 
 export const login = (data) => axios.post(`${API_BASE_URL}/auth/login`, data);
-export const register = (data) => axios.post(`${API_BASE_URL}/auth/register`, data);
+export const register = (data) =>
+  axios.post(`${API_BASE_URL}/auth/register`, data);
