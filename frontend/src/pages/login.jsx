c6d74871acf4129494553d9c072705b939589a39
BeverlyNdukwe <ndukwebeverly@gmail.com>
2025-10-30 11:55:38 +0300

    Save change in login.jsx

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -13,7 +13,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/auth/login", {
+      const response = await fetch("http://127.0.0.1:5000/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
