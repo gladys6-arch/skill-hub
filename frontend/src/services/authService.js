7ad4dd576fb48a7f899c4ed611a73637153603a6
gladys <achandogladys6@gmail.com>
2025-11-03 18:05:34 +0300

    changes in admin login

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { API_BASE_URL } from "../api";
 
-export const login = (data) => axios.post(`${API_BASE_URL}/auth/login`, data);
+export const login = (data) => axios.post(`${API_BASE_URL}/api/auth/login`, data);
 export const register = (data) =>
-  axios.post(`${API_BASE_URL}/auth/register`, data);
+  axios.post(`${API_BASE_URL}/api/auth/register`, data);
