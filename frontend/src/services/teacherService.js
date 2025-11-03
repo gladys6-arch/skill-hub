1a4655131c2cb42cde105fc524c0d54f9768ff30
gladys <achandogladys6@gmail.com>
2025-11-03 17:29:47 +0300

    Fix all hardcoded URLs

--- frontend/src/services/teacherService.js
+++ frontend/src/services/teacherService.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-
-const API_BASE_URL = "http://127.0.0.1:5000"; // your backend URL
+import { API_BASE_URL } from "../api";
 
 // -------------------- COURSES --------------------
 export const getMyCourses = () => {
